644bd69fdc3f73c49d3633a54aa3cd39ae89123a
Madhu maran <160102568+Madhumaran-25@users.noreply.github.com>
2025-05-23 08:56:56 +0530

    Update Endpoints.js

--- api/Endpoints.js
+++ api/Endpoints.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://jhdkrxefyowcvflrxycn.supabase.co/rest/v1';
-export const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoZGtyeGVmeW93Y3ZmbHJ4eWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTk3NDksImV4cCI6MjA1OTgzNTc0OX0.Y9p575Yu4apxHVGtwJifWArQ0xt4J_HhIiF6jAaMvr0'
-export const AUTHORIZATION = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoZGtyeGVmeW93Y3ZmbHJ4eWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNTk3NDksImV4cCI6MjA1OTgzNTc0OX0.Y9p575Yu4apxHVGtwJifWArQ0xt4J_HhIiF6jAaMvr0'
+API_KEY = API_KEY;
+Authorization = AUTHORIZATION;
